bebde1ce412a2d10205c8fc3039eb349ab7af15a
karanonkar <onkarkaran1@gmail.com>
2026-06-30 10:49:28 +0530

    feat: build responsive navigation system

---
+++ client/src/content/navigation.js
@@ -0,0 +1,22 @@
+export const navigation = [
+  {
+    id: 1,
+    title: "Journey",
+    href: "#journey",
+  },
+  {
+    id: 2,
+    title: "Projects",
+    href: "#projects",
+  },
+  {
+    id: 3,
+    title: "Skills",
+    href: "#skills",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    href: "#contact",
+  },
+];
